1a2736887d3cdb3547eb2825ad56aa9665be4074
Grant Chen Productivity <grantchen2021@gmail.com>
2022-05-21 14:25:03 -0400

    added some extra todos

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,10 @@ const App = () => {
       <NavBar />
       <Hero/>
       <ul>
-        <li>Maybe underline on nav bar when hovered? (for both large and mobile)</li>
-        <li>Make it more responsive now that i got breakpoints</li>
+        <li>Maybe underline and make header transition up a bit on nav bar when hovered? (for both large and mobile)</li>
+        <li>Make it more responsive now that i got breakpoints, test out the different breakpoints</li>
+        <li>Fade in hero text</li>
+        <li>Make image width/heigth 100% and apply css on its BOx instead</li>
       </ul>
       <Box height = '100vh' ></Box>
     </>

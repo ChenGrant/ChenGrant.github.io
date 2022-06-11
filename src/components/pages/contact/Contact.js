47683d7bc49afdb8d50301dc7577f2695bc95104
Grant Chen Productivity <grantchen2021@gmail.com>
2022-06-11 00:47:33 -0400

    prevented contact icon refresh on contact form submission

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,83 +1,32 @@
-import { CircularProgress, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useContext, useState } from "react";
-import { Button } from "@mui/material";
-import { Form, Formik } from "formik";
-import * as Yup from "yup";
-import FormikControl from "../../shared/formik/FormikControl";
-import emailjs from "emailjs-com";
+import React from "react";
 import { v4 as uuidv4 } from "uuid";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import ErrorIcon from "@mui/icons-material/Error";
-import AnimateOnScroll from "../../shared/AnimateOnScroll";
-import { StylingValuesContext } from "../../../contexts/StylingValues";
+import ContactForm from "./ContactForm";
 
 const contactList = [
   {
     name: "gmail",
     imageURL:
-      "https://firebasestorage.googleapis.com/v0/b/personal-website-dc932.appspot.com/o/contact%2Fgmail.png?alt=media&token=d469221a-6f22-403d-8e51-95da862e9e14",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png",
     websiteURL: "mailto:grantchen2021@gmail.com",
   },
   {
     name: "github",
-    imageURL:
-      "https://firebasestorage.googleapis.com/v0/b/personal-website-dc932.appspot.com/o/contact%2Fgithub.png?alt=media&token=6ef523e4-4f86-4ae7-9d67-4c4a8368b122",
+    imageURL: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
     websiteURL: "https://github.com/ChenGrant",
   },
   {
     name: "linkedin",
     imageURL:
-      "https://firebasestorage.googleapis.com/v0/b/personal-website-dc932.appspot.com/o/contact%2Flinkedin.png?alt=media&token=ca193b60-8863-48d1-85c8-17c1eeca100f",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png",
     websiteURL: "https://www.linkedin.com/in/grant-chen-1a96ba210/",
   },
 ];
 
 const Contact = () => {
-  const { contactFormikControlMarginBottom, contactFormikControlFontHeight } =
-    useContext(StylingValuesContext);
-
-  const [sendingEmail, setSendingEmail] = useState(false);
-  const [sendingEmailSuccess, setSendingEmailSuccess] = useState(false);
-  const [sendingEmailFailure, setSendingEmailFailure] = useState(false);
-
-  const getGmail = () => {
-    return contactList.filter((item) => item.name === "gmail")[0].websiteURL;
-  };
-
-  const contactFormInitialValues = {
-    name: "",
-    email: "",
-    message: "",
-  };
-
-  const contactFormValidationSchema = Yup.object({
-    name: Yup.string().required("Required"),
-    email: Yup.string().required("Required").email("Invalid Email"),
-    message: Yup.string().required("Required"),
-  });
-
-  const contactFormOnSubmit = async (values, formik) => {
-    const renderSendingEmailStatus = (setSendingEmailStatus) => {
-      setSendingEmail(false);
-      setSendingEmailStatus(true);
-      setTimeout(() => setSendingEmailStatus(false), 2000);
-    };
-
-    setSendingEmail(true);
-    try {
-      await emailjs.send(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-        values,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
-      );
-      formik.resetForm({ values: contactFormInitialValues });
-      renderSendingEmailStatus(setSendingEmailSuccess);
-    } catch (err) {
-      renderSendingEmailStatus(setSendingEmailFailure);
-    }
-  };
+  const getGmail = () =>
+    contactList.filter((item) => item.name === "gmail")[0].websiteURL;
 
   return (
     <Box
@@ -93,87 +42,7 @@ const Contact = () => {
       <Typography variant="h1" mb={5}>
         contact
       </Typography>
-      <Formik
-        initialValues={contactFormInitialValues}
-        validationSchema={contactFormValidationSchema}
-        onSubmit={contactFormOnSubmit}
-      >
-        {(formik) => {
-          return (
-            <Form>
-              <Box
-                display="flex"
-                flexDirection="column"
-                width="min(80vw, 400px)"
-                alignItems="center"
-              >
-                <AnimateOnScroll animation="fade-left">
-                  <FormikControl
-                    control="input"
-                    label="Name"
-                    name="name"
-                    type="text"
-                    mb={
-                      formik.errors.name && formik.touched.name
-                        ? contactFormikControlMarginBottom
-                        : `calc(${contactFormikControlMarginBottom} + ${contactFormikControlFontHeight})`
-                    }
-                    disabled={sendingEmail}
-                    sendingEmail={sendingEmail}
-                  />
-                </AnimateOnScroll>
-                <AnimateOnScroll animation="fade-left">
-                  <FormikControl
-                    control="input"
-                    label="Email"
-                    name="email"
-                    type="email"
-                    mb={
-                      formik.errors.email && formik.touched.email
-                        ? contactFormikControlMarginBottom
-                        : `calc(${contactFormikControlMarginBottom} + ${contactFormikControlFontHeight})`
-                    }
-                    sendingEmail={sendingEmail}
-                  />
-                </AnimateOnScroll>
-                <AnimateOnScroll animation="fade-left">
-                  <FormikControl
-                    control="textarea"
-                    label="Message"
-                    name="message"
-                    mb={
-                      formik.errors.message && formik.touched.message
-                        ? contactFormikControlMarginBottom
-                        : `calc(${contactFormikControlMarginBottom} + ${contactFormikControlFontHeight})`
-                    }
-                    sendingEmail={sendingEmail}
-                  />
-                </AnimateOnScroll>
-                <Box height="60px" display="flex" justifyContent="center">
-                  {sendingEmail ? (
-                    <CircularProgress color="secondary" />
-                  ) : sendingEmailSuccess ? (
-                    <CheckCircleIcon color="success" sx={{ fontSize: 50 }} />
-                  ) : sendingEmailFailure ? (
-                    <ErrorIcon color="error" sx={{ fontSize: 50 }} />
-                  ) : (
-                    <AnimateOnScroll animation="fade-left">
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        type="submit"
-                        sx={{ borderRadius: "9px" }}
-                      >
-                        Submit
-                      </Button>
-                    </AnimateOnScroll>
-                  )}
-                </Box>
-              </Box>
-            </Form>
-          );
-        }}
-      </Formik>
+      <ContactForm />
       <Box my={4} mt={10} height="40px" display="flex" gap="50px">
         {contactList.map((contact) => {
           const { name, imageURL, websiteURL } = contact;
@@ -189,10 +58,11 @@ const Contact = () => {
                   top: "-10px",
                 },
               }}
+              component="a"
+              href={websiteURL}
+              target="_blank"
             >
-              <Box component="a" href={websiteURL} target="_blank">
-                <img height="100%" src={imageURL} alt={name} />
-              </Box>
+              <img height="100%" src={imageURL} alt={name} />
             </Box>
           );
         })}

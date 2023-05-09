bc283158b92f3c55afca90ae09f875d09accf3cf
Productivity <grantchen2021@gmail.com>
2023-05-08 22:25:48 -0400

    updated resume to have linux

--- src/components/pages/navBar/NavBarItem.js
+++ src/components/pages/navBar/NavBarItem.js
@@ -10,7 +10,7 @@ const grow = keyframes({ from: { width: "0%" }, to: { width: "100%" } });
 const shrink = keyframes({ from: { width: "100%" }, to: { width: "0%" } });
 
 const resumeSrc =
-  "https://firebasestorage.googleapis.com/v0/b/personal-website-dc932.appspot.com/o/resume%2FGrant%20Chen%20Resume.pdf?alt=media&token=3da515b0-6dac-4ac5-bf3f-cc66736aec01";
+  "https://firebasestorage.googleapis.com/v0/b/personal-website-dc932.appspot.com/o/resume%2FGrant%20Chen%20Resume.pdf?alt=media&token=be75d594-be65-4a15-ad42-cd5694828f68";
 
 const NavBarItem = ({ children, link, resume, closeMenu }) => {
   const [navBarItemIsHovered, setNavBarItemIsHovered] = useState(false);
